8137639cd8b0098b67240cd34850d6a62abb3f68
kimjcob <kimbyungwhi@gmail.com>
2024-08-14 22:16:24 -0700

    Update PORT variable to use default value of 27017

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const app = express();
-const PORT = process.env.port || 5500;
+const PORT = 27017;
 const expenseRoutes = require('./routes/expense')
 const path = require('path');
 
